a6fa4df7fc108dfb7be79608e59e8eda58d2d44f
GabrielyNanci <fornancy14@gmail.com>
2021-06-22 17:04:09 -0300

    Esse codigo foi postado no AwardSpace, porém, não funcionou em nenhum navegador que tenho disponivél, funionou apenas no localhost.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 
-//navigator.geolocation.getCurrentPosition(print_geo);
+//Esse codigo foi postado no AwardSpace, porém, não funcionou em nenhum navegador que tenho disponivél,
+//funiona apenas no localhost.//
+
+navigator.geolocation.getCurrentPosition(print_geo);
 
 function print_geo(){
     navigator.geolocation.getCurrentPosition(function(position){
@@ -10,6 +13,7 @@ function print_geo(){
 function atual(){;
   navigator.geolocation.getCurrentPosition(function(position){
    document.getElementById("tela2").value = position.coords.latitude
+//Essa parte não funciona, escreve NaN, não sei o motivo, sou muito burra e o zero vem ;_;//
     document.getElementById ("tela3").value = parseFloat("print_geo") - parseFloat("atual") ;
 })
 }
